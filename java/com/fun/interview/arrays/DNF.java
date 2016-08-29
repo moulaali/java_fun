d0de61b174fa2339e98c705e65e844ff802eefc7
moulaali <hi.moulaali@gmail.com>
2016-08-29 16:17:36 -0700

    Update DNF.java
    Added expected output

--- java/com/fun/interview/arrays/DNF.java
+++ java/com/fun/interview/arrays/DNF.java
@@ -13,6 +13,7 @@ public class DNF {
   public static void main(String[] args) {
     int[] input = new int[] {0, 1, 2, 1, 1, 2, 2, 0};
     doDnf(input);
+    // This should print : Array after partitoning : [0, 0, 1, 1, 1, 2, 2, 2]
     System.out.println("Array after partitoning : " + Arrays.toString(input));
   }
 
