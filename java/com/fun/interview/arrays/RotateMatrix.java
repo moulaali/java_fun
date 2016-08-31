e734069364657e4ff9307c79dc80f80c6d4058d4
moulaali <hi.moulaali@gmail.com>
2016-08-31 12:03:39 -0700

    Update RotateMatrix.java

--- java/com/fun/interview/arrays/RotateMatrix.java
+++ java/com/fun/interview/arrays/RotateMatrix.java
@@ -13,7 +13,10 @@ public class RotateMatrix {
   public static void main(String[] args) throws Exception {
     int[][] input =
         new int[][] {
-          {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16},
+          {1, 2, 3, 4},
+          {5, 6, 7, 8},
+          {9, 10, 11, 12},
+          {13, 14, 15, 16},
         };
 
     rotate(input);
