999c4c0a7125ce3c59d36e19028ecdee12524442
moulaali <hi.moulaali@gmail.com>
2019-04-12 12:56:55 -0700

    Update AddNumbersAsLinkedList.java

--- java/com/fun/interview/arrays/AddNumbersAsLinkedList.java
+++ java/com/fun/interview/arrays/AddNumbersAsLinkedList.java
@@ -1,99 +1,122 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
- * You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+ You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
 
-    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
-    Example
-    
-    Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-    Output: 7 -> 0 -> 8
-    Explanation: 342 + 465 = 807.
-    
-    https://leetcode.com/problems/add-two-numbers/description/
+ Example:
+
+ Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ Output: 7 -> 0 -> 8
+ Explanation: 342 + 465 = 807.
  */
- 
-class AddNumbersAsLinkedList
-{
-   
-    static void printMiinWind(String S, String T) {
+public class AddNumsAsList {
 
-    }
- 
-    public static void main(String[] args) 
-    {
-        // 100
-        Node head = new Node(0);
-        Node first = new Node(0);
-        Node second = new Node(1);
-        head.next = first;
-        first.next = second;
-        head.printChain();
-        
-        // 111
-        Node head2 = new Node(1);
-        Node first2 = new Node(1);
-        Node second2 = new Node(1);
-        head2.next = first2;
-        first2.next = second2;
-        head2.printChain();
-        
-        add(head, head2).printChain();
-    }
-    
-    static Node add(Node head, Node head2) {
-        Node curr1 = head;
-        Node curr2 = head2;
-        
-        Node sum = null;
-        
-        boolean first = true;
-        int carry = 0;
-        Node prev = null;
-        
-        while((curr1 != null) || (curr2 != null) || (carry != 0)) {
-            int curr1data = (curr1 != null ? curr1.data : 0);
-            int curr2data = (curr2 != null ? curr2.data : 0);
-            
-            Node n = new Node(((curr1data + curr2data + carry) % 10));
-            carry = (curr1data + curr2data + carry) / 10;
-            
-            if (first) {
-                sum = n;
-                first = false;
-            } else {
-                prev.next = n;      
-            }
-            
-            prev = n;
-            if (curr1 != null) {
-                curr1 = curr1.next;
-            }
-            if (curr2 != null) {
-                curr2 = curr2.next;
-            }
-        }
-        
-        return sum;
-    }
-    
+  static class Node {
+    int data;
+    Node next;
 
-    static class Node {
-        int data;
-        Node next;
-        
-        Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-        
-        void printChain() {
-            Node curr = this;
-            
-            for (;curr != null; curr= curr.next) {
-                System.out.print(curr.data + "->");
-            }
-            
-            System.out.println("null");
-        }
+    Node(int data) {
+      this.data = data;
+      this.next = null;
     }
+
+    void print() {
+      System.out.print(this.data + "->");
+      if (this.next == null) {
+        System.out.print("null");
+      } else {
+        this.next.print();
+      }
+    }
+
+    Node append(int data) {
+      Node n = new Node(data);
+      this.next = n;
+      return n;
+    }
+
+    Node append(Node n) {
+      this.next = n;
+      return n;
+    }
+  }
+
+  static Node add(Node first, Node second) {
+    Node sum = null;
+    Node sumTail = null;
+    int carry = 0;
+
+    Node temp1 = first;
+    Node temp2 = second;
+
+    while (temp1 != null && temp2 != null) {
+      int sumInt = (temp1.data + temp2.data + carry) % 10;
+      carry = (temp1.data + temp2.data + carry) / 10;
+
+      if (sum == null) {
+        System.out.println("Adding new (first) node to sum " + sumInt);
+        sum = new Node (sumInt);
+        sumTail = sum;
+      } else {
+        System.out.println("Adding new node to sum " + sumInt);
+        Node n = new Node(sumInt);
+        sumTail.append(n);
+        sumTail = n;
+      }
+
+      temp1 = temp1.next;
+      temp2 = temp2.next;
+    }
+
+    // left over digits
+    Node leftOver = null;
+    if (temp1 != null) {
+      leftOver = temp1;
+    }
+
+    if (temp2 != null) {
+      leftOver = temp2;
+    }
+
+    if (leftOver == null) {
+      return sum;
+    }
+
+    while (leftOver != null) {
+      int sumInt = (leftOver.data + carry) % 10;
+      carry = (leftOver.data + carry) / 10;
+      System.out.println("Adding new node (leftover) to sum " + sumInt);
+
+      Node n = new Node(sumInt);
+      sumTail.append(n);
+      sumTail = n;
+
+      leftOver = leftOver.next;
+    }
+
+    // add carry over
+    if (carry > 0) {
+      Node n = new Node(carry);
+      sumTail.append(n);
+    }
+
+    return sum;
+  }
+
+  public static void main(String[] args) {
+    Node first = new Node(2);
+    first.append(4).append(3).append(4);
+    first.print();
+    System.out.println("\n");
+
+    Node second = new Node(5);
+    second.append(6).append(4).append(9).append(9);
+    second.print();
+    System.out.println("\n");
+
+    add(first, second).print();
+  }
 }
